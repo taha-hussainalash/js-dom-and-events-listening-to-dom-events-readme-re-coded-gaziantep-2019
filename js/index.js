88e0f8c97070963d6e84d0f3272bda6e46bf11ac
Taha HussainAlash <taha.benzma94@gmail.com>
2019-08-17 10:04:15 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,37 @@
 // using this file is optional
 // you can also load the code snippets in using your browser's console
+
+
+ main = document.getElementById('main');
+ 
+main.addEventListener('click', function(event) {
+  alert('I was clicked!');
+});
+
+const input = document.querySelector('input');
+ 
+input.addEventListener('keydown', function(e) {
+  console.log(e.which);
+});
+
+const input = document.querySelector('input');
+ 
+input.addEventListener('keydown', function(e) {
+  if (e.which === 71) {
+    console.log('default prevented');
+    return e.preventDefault();
+  } else {
+    console.log('Not a "g"');
+  }
+});
+
+let divs = document.querySelectorAll('div');
+ 
+function bubble(e) {
+  console.log(this.firstChild.nodeValue.trim() + ' bubbled');
+}
+ 
+for (let i = 0; i < divs.length; i++) {
+  divs[i].addEventListener('click', bubble);
+}
+  
