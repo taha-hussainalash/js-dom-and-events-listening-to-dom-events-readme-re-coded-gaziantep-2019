1a1133be9751dd7b3364dcbb32b65028ae91ed20
Taha HussainAlash <taha.benzma94@gmail.com>
2019-08-17 10:04:05 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -28,4 +28,10 @@ input.addEventListener('keydown', function(e) {
 let divs = document.querySelectorAll('div');
  
 function bubble(e) {
+  console.log(this.firstChild.nodeValue.trim() + ' bubbled');
+}
+ 
+for (let i = 0; i < divs.length; i++) {
+  divs[i].addEventListener('click', bubble);
+}
   
