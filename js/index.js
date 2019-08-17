8d63c4f9494ad1241fa98260ad5721839db4f1d3
Taha HussainAlash <taha.benzma94@gmail.com>
2019-08-17 10:02:42 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -14,3 +14,13 @@ input.addEventListener('keydown', function(e) {
   console.log(e.which);
 });
 
+const input = document.querySelector('input');
+ 
+input.addEventListener('keydown', function(e) {
+  if (e.which === 71) {
+    console.log('default prevented');
+    return e.preventDefault();
+  } else {
+    console.log('Not a "g"');
+  }
+});
