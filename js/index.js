d4c6db9eb68880a31c4f8661d2c63b89de270177
Taha HussainAlash <taha.benzma94@gmail.com>
2019-08-17 10:02:01 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -7,3 +7,9 @@
 main.addEventListener('click', function(event) {
   alert('I was clicked!');
 });
+
+const input = document.querySelector('input');
+ 
+input.addEventListener('keydown', function(e) {
+  console.log(e.which);
+});
